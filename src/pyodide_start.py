ac43c54be055885992797751aa099e6de82e2c29
Lucas Ikuhara <lri2911@gmail.com>
2023-02-10 12:49:49 -0300

    feat: add pyodide API proxy

--- src/pyodide_start.py
+++ src/pyodide_start.py
@@ -1,18 +1,71 @@
 
 # All missing imports below are handled by Pyodide
 
-def for_all_methods(decorator):
-    def decorate(cls):
-        for attr in cls.__dict__: # there's propably a better way to do this
-            if callable(getattr(cls, attr)):
-                setattr(cls, attr, decorator(getattr(cls, attr)))
-        return cls
-    return decorate
-
-@for_all_methods(graph_as_json)
-class FigureAsJSON(FigureFactory):
-    pass
+import json
 
 
-telemetry = HistoricTelemetry(100)
-map = MapState()
+class GraphJsProxy:
+    '''
+    This is a mock to the entire API, which is meant to be used deployed on the edge with pyodide.
+    '''
+
+    def __init__(self):
+        self.telemetry = HistoricTelemetry(100)
+        self.radar = MapState()
+
+    def state_from_json(self, value):
+        state = json.loads(value)
+        self.telemetry.state_from_json(state)
+
+    def indicators_from_json(self, value):
+        indicators = json.loads(value)
+        self.telemetry.indicators_from_json(indicators)
+
+    def objects_from_json(self, value):
+        indicators = json.loads(value)
+        self.radar.objects_from_json(indicators)
+
+    @graph_as_json
+    def speed_over_time(self):
+
+        return FigureFactory.speed_series(
+            self.telemetry.speed_series,
+            self.telemetry.time_series
+        )
+
+    @graph_as_json
+    def altitude_over_time(self):
+
+        return FigureFactory.altitude_series(
+            self.telemetry.altitude_series,
+            self.telemetry.time_series
+        )
+
+    @graph_as_json
+    def energy_over_time(self):
+
+        return FigureFactory.sme_series(
+            self.telemetry.speed_series,
+            self.telemetry.altitude_series,
+            self.telemetry.time_series
+        )
+
+    @graph_as_json
+    def artificial_horizon(self):
+
+        return FigureFactory.artificial_horizon(
+            self.telemetry.roll,
+            self.telemetry.pitch,
+            self.telemetry.compass
+        )
+
+    @graph_as_json
+    def radar_map(self):
+
+        return FigureFactory.radar_map(
+            self.radar,
+            self.telemetry.compass
+        )
+
+
+python_proxies = GraphJsProxy()
