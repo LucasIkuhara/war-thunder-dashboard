cacd359e0406589b2189b5d9b6b3114dcc95df69
Lucas Ikuhara <lri2911@gmail.com>
2024-04-18 14:22:27 -0300

    fix: service worker path

--- src/pwa_install.js
+++ src/pwa_install.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register("/service_worker.js");
+    navigator.serviceWorker.register("src/service_worker.js");
 }
 else {
     console.log("Service worker API is not available in the current context.");
